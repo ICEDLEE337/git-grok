e50f785b0abcd25945986b68acccea5cf58c7ed7
Lee Norris <lee.norris@arcadia.io>
2019-10-22 22:24:14 -0400

    remove dead code

--- src/lib/result-transformer.ts
+++ src/lib/result-transformer.ts
@@ -3,30 +3,14 @@ import _ from 'lodash';
 import RepoSearchResultWithFileDetails from './models/repo-search-result-with-file-details';
 import FileSearchResult from './models/file-search-result';
 import RepoSearchResult from './models/repo-search-result';
-// import FileSearchResult from './models/file-search-result';
 
 export default class ResultTransformer {
-
-    static transformEventually(repoSearchResult: RepoSearchResult): RepoSearchResultWithFileDetails {
-        const fileSearchResults: any = _((repoSearchResult.matches || '').split('\n'))
-            .filter(_.partialRight(_.includes, ':'))
-            // .map(line => line && console.log(line) || line)
-            .groupBy((line: string) => line.split(':')[0])
-            // .map(groups => console.log(JSON.stringify(groups, null, 2)))
-            .value();
-
-        return fileSearchResults;
-    }
-
-    static transformNoop(result: any): any {
-        return _.identity(result);
-    }
 
     static transform(matches: string, repo: string): RepoSearchResultWithFileDetails {
         return this.transformInternal({matches, repo});
     }
 
-    static transformInternal(result: RepoSearchResult): RepoSearchResultWithFileDetails {
+    private static transformInternal(result: RepoSearchResult): RepoSearchResultWithFileDetails {
         const matchesRefined = _(result.matches.split('\n\n')).map(this.splitFileChunk).value();
         const transformed: RepoSearchResultWithFileDetails = _.assign({}, result,
             {
