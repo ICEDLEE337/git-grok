fc5085fbac1e1db97ad16cab29968e11dbf1d63c
Lee Norris <lee.norris@arcadia.io>
2020-02-07 22:05:49 -0500

    chore: add scm domain name param to path manager

--- src/lib/path-manager.ts
+++ src/lib/path-manager.ts
@@ -2,12 +2,12 @@ import { exec } from "child_process";
 import { resolve } from "path";
 
 export class PathManager {
-    static getHomeDirectory(): Promise<string> {
+    static getHomeDirectory(scmProviderPath?: string): Promise<string> {
         return new Promise(r => {
             exec('echo $HOME', (_err, stdout) => {
                 r(resolve(stdout.toString().split('\n')[0]));
             });
         })
-        .then(home => `${home}/.gitgrok/`.replace('//', '/'));
+        .then(home => `${home}/.gitgrok/${scmProviderPath || ''}/`.replace('//', '/'));
     }
 }
