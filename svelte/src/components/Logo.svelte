8168800feb9ef9c069eac47c0ba67fd87f0a4083
Lee Norris <lee.norris@arcadia.io>
2020-02-12 21:09:49 -0500

    feat: logo

---
+++ svelte/src/components/Logo.svelte
@@ -0,0 +1,24 @@
+<style type="text/scss">
+
+figure {
+    display: flex;
+    justify-content: stretch;
+    background-color: var(--vscode-button-foreground);
+}
+
+.git, .grok {
+    margin: 0;
+}
+
+.git {
+    color: var(--vscode-button-background);
+}
+
+</style>
+
+
+<figure>
+<figurecaption>
+<span class="git subtitle">git</span><span class="grok title">Grok</span>
+</figurecaption>
+</figure>
--- svelte/src/components/Menu.svelte
+++ svelte/src/components/Menu.svelte
@@ -3,9 +3,10 @@ import Button from './../components/Btn.svelte';
 import {routerStore} from '../stores/router.store';
 import Search from '../components/Search.svelte';
 import Clone from '../components/Clone.svelte';
+import Logo from '../components/Logo.svelte';
 import RepoList from '../components/RepoList.svelte';
 
-let active;
+let activeName;
 
 let links = [
     {name: 'Search', component: Search},
@@ -19,23 +20,44 @@ function nav (link) {
 }
 
 function setActive (link) {
-    active = link.name;
+    activeName = link.name;
 }
 
 </script>
 
-<style type="text/scss">
-@import '../styles/_variables.scss';
-.menu {
-    display: flex;
-    justify-content: stretch;
-    flex-direction: column;
-}
-</style>
 
-<div class="menu">
-    {#each links as link}
-        <Button appearance={link.name === active ? 'active' : ''} label={link.name} on:click={() => nav(link)} />
-    {/each}
-</div>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <div class="navbar-item">
+        <Logo />
+    </div>
+
+    <a role="button" href="/" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+        {#each links as link}
+            <button class="navbar-item" class:isPrimary="{link == activeName}" on:click={() => nav(link)}>{link.name}</button>
+        {/each}
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <button class="button is-primary">
+            <strong>Sign up</strong>
+          </button>
+          <button class="button is-light">
+            Log in
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
 
