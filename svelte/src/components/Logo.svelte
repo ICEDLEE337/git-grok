a172bd9bb42cd444df97e3a09fc201fdbda264b4
Lee Norris <lee.norris@arcadia.io>
2020-02-21 17:53:23 -0500

    chore: revert logo

--- svelte/src/components/Logo.svelte
+++ svelte/src/components/Logo.svelte
@@ -13,11 +13,12 @@ figure {
 .git {
     color: var(--vscode-button-background);
 }
+
 </style>
 
 
 <figure>
 <figurecaption>
- <a href="https://github.com/icedlee337/git-grok"><span class="git subtitle">git</span><span class="grok title">Grok</span><span class="git subtitle">&gt;</span></a>
+<span class="git subtitle">git</span><span class="grok title">Grok</span><span class="git subtitle">&gt;</span>
 </figurecaption>
 </figure>
